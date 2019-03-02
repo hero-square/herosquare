f1904fd6332e0af551fdf7c4d88a372935da87a2
Adrian <34041016+Sgshi@users.noreply.github.com>
2019-03-01 18:58:56 -0800

    reorganize files and css, added animated hamburger. add is-active class to animate

---
+++ herosquare/src/components/App.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../style/App.css';
+import '../style/hamburger.css';
+import Authen from './Authen';
+
+//change this to be better later
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App__header">
+
+          {/* append is-active to animate hamburger */}
+          <button className="hamburger hamburger--elastic" type="button">
+            <span className="hamburger-box">
+              <span className="hamburger-inner" />
+            </span>
+          </button>
+
+          <img className="App__logo" src={logo} alt="logo" />
+          <h2 className="App__title"> HeroSquare </h2>
+        </header>
+        <Authen />
+      </div>
+    );
+  }
+}
+
+export default App;
