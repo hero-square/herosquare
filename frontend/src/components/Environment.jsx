4cfd5d8362e6e3928c9ffaead9fdc373b88c64c3
Adrian <34041016+Sgshi@users.noreply.github.com>
2019-03-19 22:45:36 -0700

    mock data from db

--- frontend/src/components/Environment.jsx
+++ frontend/src/components/Environment.jsx
@@ -1,20 +1,40 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import EnvironmentLarge from './EnvironmentLarge';
 import EnvironmentSmall from './EnvironmentSmall';
 import { Redirect } from 'react-router-dom';
 
 const Environment = props => {
+  if (!isLoggedIn) return <Redirect to="/" />;
+
   const [smallScreen, useSmallScreen] = useState(false);
+  const [userInfo, useUserInfo] = useState({});
   const { match, isLoggedIn } = props;
 
-  if (!isLoggedIn) return (<Redirect to= '/' />);
+  useEffect(() => {
+    // api call for user data
+    useUserInfo({
+      userID: 0,
+      name: 'PersonMcPersonFace',
+      classes: {
+        0: {
+          schoolID: 0,
+          classID: 0,
+          sectionID: 0,
+          className: 'classyMcClassface',
+          time: new Date(),
+          professor: 'TeacheryMcTeacherFace',
+        },
+      },
+      picture: '',
+    });
+  });
 
   return (
     <>
       {smallScreen ? (
-        <EnvironmentSmall match={match} />
+        <EnvironmentSmall match={match} userInfo={userInfo} />
       ) : (
-        <EnvironmentLarge match={match} />
+        <EnvironmentLarge match={match} userInfo={userInfo} />
       )}
     </>
   );
