b49374f15d4efced259a5e60049828abed1fe176
Adrian <34041016+Sgshi@users.noreply.github.com>
2019-03-19 17:04:38 -0700

    added mobile menu, debating whether to bake into menu css

---
+++ frontend/src/components/mobileMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//? goes where logo is if on mobile, has same functionality as menu
+//? debating whether to bake into Menu.jsx css
+
+const mobileMenu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default mobileMenu
