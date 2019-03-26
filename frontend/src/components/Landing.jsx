8f9b35d7fea9f7f18c94cd5bca2fbfe313a4288a
Adrian <34041016+Sgshi@users.noreply.github.com>
2019-03-26 01:00:57 -0700

    added loading icon and updated landing page to accept populararticles

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { LandingHeader } from './Headers';
+import PopularArticles from './PopularArticles';
+import '../style/landing.css';
 
 const Landing = props => {
   return (
     <div>
-      <h1>this is home page</h1>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
+      <LandingHeader />
+      <div className="landing">
+        <PopularArticles />
+        <h1>this is home page</h1>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+      </div>
     </div>
   );
 };
