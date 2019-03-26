12859d1327bcc1b30854d53e0c4b857860012bb0
Adrian <34041016+Sgshi@users.noreply.github.com>
2019-03-25 22:56:35 -0700

    login form works

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,32 +3,44 @@ import { Redirect } from 'react-router-dom';
 
 const Login = props => {
   const { setAuthStatus, authStatus } = props;
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState('text');
+  const [password, setPassword] = useState('here');
 
   if (authStatus) return <Redirect to="/home" />;
+
+  const handleForm = e => {
+    e.preventDefault();
+    //* hash passwords and other security here
+    // should stick to only oauth for security purposes
+    console.log(username);
+    console.log(password);
+    
+    //authentication request, if response...
+    setAuthStatus(true);
+  };
 
   return (
     <div>
       <h1>Welcome to HeroSquare</h1>
-      <input
-        name="username"
-        type="text"
-        value={username}
-        onChange={e => setUsername(e.value)}
-      />
-      <input
-        name="pass"
-        type="password"
-        value={password}
-        onChange={e => setPassword(e.value)}
-      />
-      <button
-        onClick={() => {
-          setAuthStatus(true);
-        }}>
-        Click button to log in for now
-      </button>
+      <form onSubmit={handleForm}>
+        <input
+          type="text"
+          required
+          name='username'
+          placeholder='Username'
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          required
+          name='password'
+          placeholder='Password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button type='submit'>Log In</button>
+      </form>
       <button onClick={() => <Redirect to="/signup" />}>signup</button>
       <button onClick={() => <Redirect to="/retrievePassword" />}>
         forgot password
