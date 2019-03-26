e892013a3a4a627f4cdcf2df88dc028eba30d1c8
Adrian <34041016+Sgshi@users.noreply.github.com>
2019-03-26 01:00:08 -0700

    added popular article component to display relevant info on landing page

---
+++ frontend/src/components/PopularArticleItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const PopularArticleItem = props => {
+  const { title, image, description } = props.article;
+  const { position } = props;
+  return (
+    <div
+      className={`popular-article--item ${
+        position === 1 || position === 6
+          ? 'popular-article--lrg'
+          : 'popular-article--sml'
+      }`}>
+      <img src={image} alt="image" />
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default PopularArticleItem;
